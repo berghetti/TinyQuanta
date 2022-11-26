fa4d62705e184cfd29ae0748705030b9f6a3962e
zhluo94 <zhluo@berkeley.edu>
2022-11-26 06:53:03 +0000

    nit

--- set_cpu_mode.sh
+++ set_cpu_mode.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-for cpu_id in {28..33}
-do
-	echo "performance" | sudo tee /sys/devices/system/cpu/cpu${cpu_id}/cpufreq/scaling_governor
-done
+echo "performance" | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
