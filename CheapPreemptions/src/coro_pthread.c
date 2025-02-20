b6b1c03f4cfe5f1a50139931cc0de5a40e1c5ebb
berghetti <mayco.s.berghetti@gmail.com>
2025-02-20 08:20:19 -0500

    go back core_pthread

--- CheapPreemptions/src/coro_pthread.c
+++ CheapPreemptions/src/coro_pthread.c
@@ -5,15 +5,15 @@
 int __wrap_pthread_mutex_lock(pthread_mutex_t *mutex) {
   int ret;
   for(;;) {
-    //if (mutex->__data.__owner == cp_pid) {
-    //    // mutex is held by the same thread, yield
-    //    (*intvActionHook)(0);
-    //    continue;
-    //}
+    if (mutex->__data.__owner == cp_pid) {
+        // mutex is held by the same thread, yield
+        (*intvActionHook)(0);
+        continue;
+    }
     ret = pthread_mutex_trylock(mutex);
     if(ret == EBUSY) {
         // mutex is held, yield
-    //    (*intvActionHook)(ret);
+        (*intvActionHook)(ret);
         continue;
     }
     // for other cases, we are done
