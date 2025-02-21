57e4418494be6716bfa95ba0a274a11d312ce386
berghetti <mayco.s.berghetti@gmail.com>
2025-02-21 09:30:34 -0500

    fix passe

--- leveldb/tq.mk
+++ leveldb/tq.mk
@@ -10,25 +10,27 @@ OPT = opt-$(LLVM_VERSION)
 LLVM_AR := llvm-ar-$(LLVM_VERSION)
 CXX := clang++-$(LLVM_VERSION)
 
-CXX_FLAGS := -emit-llvm -g -S -O3 -I. -I./include -pthread -DOS_LINUX -DLEVELDB_PLATFORM_POSIX -DSNAPPY -DNDEBUG -c -fPIC
+CXX_FLAGS := -emit-llvm -g -S -O3 -I. -I./include -pthread -DOS_LINUX -DLEVELDB_PLATFORM_POSIX -DSNAPPY -DNDEBUG -fPIC
 
-TQ_MAIN=$(ROOT_DIR)..
+ROOT_DIR:=$(shell dirname $(realpath $(firstword $(MAKEFILE_LIST))))
+
+TQ_MAIN=$(ROOT_DIR)/..
 
 CP_PASS=$(TQ_MAIN)/CheapPreemptions/lib/CheapPreemption.so
 
-#OPT_CONFIG = -postdomtree -mem2reg -indvars -loop-simplify -branch-prob -scalar-evolution
 OPT_FLAGS = -strip-debug -postdomtree -mem2reg -indvars -loop-simplify -branch-prob -scalar-evolution
+#OPT_FLAGS = -postdomtree -mem2reg -indvars -loop-simplify -branch-prob -scalar-evolution
 ARFLAGS = -rs
 
 CFLAGS_CP = -Wno-register -O3 -DNDEBUG
 
-CP_FLAGS = -load $(CP_PASS) -cheap_preempt
 CMT_INTV ?= 800
 EXT_COST ?= 1
 MAX_E2E  ?= 1000
-#FUNC_THRE ?= 100000000
+FUNC_THRE ?= 100000000
 # taken from ../Makefile
-FUNC_THRE ?= 60
+#FUNC_THRE ?= 60
+CP_FLAGS = -load $(CP_PASS) -cheap_preempt
 CP_FLAGS += -commit-intv=$(CMT_INTV) -ext-lib-cost=$(EXT_COST) -max-e2e-length=$(MAX_E2E) -func-call-threshold=$(FUNC_THRE) -will-update-last-cycle-ts
 
 all: clean build_db build_util build_table
@@ -38,10 +40,16 @@ tq_pass:
 	llvm-link-$(LLVM_VERSION) -o libleveldb.bc $(BUILD_DIR)/*.bc
 	llvm-dis-$(LLVM_VERSION) libleveldb.bc -o leveldb.ll
 
-	$(OPT) $(OPT_FLAGS) -S < leveldb.ll > leveldb.opt.ll
-	$(OPT) $(CP_FLAGS) -S < leveldb.opt.ll > leveldb_cp.ll
-	$(CXX) -c leveldb_cp.ll -o leveldb.o $(CFLAGS_CP) -fPIC -flto
-	$(LLVM_AR) $(ARFLAGS) libleveldb_tq.a leveldb.o
+	$(OPT) -S $(OPT_FLAGS) < leveldb.ll > leveldb.opt.ll
+
+	$(OPT) -S $(CP_FLAGS) < leveldb.opt.ll > leveldb_cp.ll
+# cheap_preempt
+	$(CXX) -c -O3 -fPIC leveldb_cp.ll -o leveldb_cp.o
+# clear
+	$(CXX) -c -O3 -fPIC leveldb.opt.ll -o leveldb.o
+	
+	$(LLVM_AR) $(ARFLAGS) libleveldb_tq.a leveldb_cp.o
+	$(LLVM_AR) $(ARFLAGS) libleveldb_clear.a leveldb.o
 
 build_db:
 	@mkdir -p $(BUILD_DIR)
